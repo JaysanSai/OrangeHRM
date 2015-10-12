3ae92074befa9071194e61d556c37129601126aa
JaysanSai <jaysanias@gmail.com>
2015-10-12 11:24:32 -0400

    addEmployee feature file and AddEmployee step definition file are updated with some initial code

--- src/test/java/stepDefinitions/AddEmployee.java
+++ src/test/java/stepDefinitions/AddEmployee.java
@@ -1,5 +1,61 @@
 package stepDefinitions;
 
+import java.util.List;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class AddEmployee {
+	Login stepsLogin= new Login();
+	
+	@Given("^I am in OrangeHRM dashboard page$")
+	public void i_am_in_OrangeHRM_dashboard_page(List<List<String>> data) throws Throwable {
+		List<String> url=data.get(0);
+		String username=data.get(1).get(1);
+		String password=data.get(2).get(1);
+		stepsLogin.i_am_in_OrangeHRM_login_page(url);
+		stepsLogin.i_entered_admin_username_as_and_password_as(username, password);
+		stepsLogin.i_clicked_on_Login_Button();
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new PendingException();
+	}
+
+	@When("^I clicked on the PIM menu$")
+	public void i_clicked_on_the_PIM_menu() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	  //  throw new PendingException();
+	}
+
+	@And("^I clicked on the add employee sub-menu$")
+	public void i_clicked_on_the_add_employee_sub_menu() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new PendingException();
+	}
+
+	@Then("^Add Employee page is successfully displayed$")
+	public void add_Employee_page_is_successfully_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new PendingException();
+	}
+
+	@Given("^I entered First Name as?(.*), Middle Name as?(.*) and Last Name as?(.*)$")
+	public void i_entered_Employee_full_Names(String firstName,String middleName, String lastName) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new PendingException();
+	}
+
+	@When("^I clicked the save button$")
+	public void i_clicked_the_save_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new PendingException();
+	}
+
+	@Then("^Employee added successfully$")
+	public void employee_added_successfully() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new PendingException();
+	}
 
 }
--- src/test/resources/features/addEmployee.feature
+++ src/test/resources/features/addEmployee.feature
@@ -1,8 +1,27 @@
 Feature: Add New Employee in the database
-	This will automate add new employee feature of the system
-	Background: Login as Admin
-	Given I am in OrangeHRM login page
-      | url | http://enterprise.demo.orangehrmlive.com |
-	Scenario: Add New Employee
-	
-	When 
+  This will automate add new employee feature of the system
+
+  Background: Go to Add Employee page
+    
+    #Below given statement contains duplication as url, username and password are
+    #declared again on this feature file while they were already declared in login feature file
+    #Question: Is there a way a scenarion written in another feature file can be called 
+    #this feature file in Cucumber?
+    Given I am in OrangeHRM dashboard page
+      | url      | http://enterprise.demo.orangehrmlive.com |
+      | username | Admin                                    |
+      | password | admin                                    |
+    When I clicked on the PIM menu
+    And I clicked on the add employee sub-menu
+    Then Add Employee page is successfully displayed
+
+  Scenario Outline: Add New Employees
+    Given I entered First Name as <First Name>, Middle Name as <Middle Name> and Last Name as <Last Name>
+    When I clicked the save button
+    Then Employee added successfully
+
+    Examples: 
+      | First Name | Middle Name | Last Name |
+      | Jay        |             | Yes       |
+      | Kiran      | Lal         | Gurung    |
+      | Jon        | K           | Rice      |
